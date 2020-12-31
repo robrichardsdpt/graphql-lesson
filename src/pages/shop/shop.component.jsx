5664f43c768b78d79d5686c91bf656d4c285191b
Rob <robrichardsdpt@gmail.com>
2020-12-31 14:59:08 -0500

    toggle cart hidden now works with apollo instead of redux

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-
-import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
-import CollectionPage from '../collection/collection.component';
+import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container'
+import { default as CollectionPage } from '../collection/collection.container';
 
 const ShopPage = ({ match }) => (
   <div className='shop-page'>
-    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route exact path={`${match.path}`} component={CollectionsOverviewContainer} />
     <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
   </div>
 );
